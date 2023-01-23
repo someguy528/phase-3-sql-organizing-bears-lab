27ac438912ba192ba5ff86a1e5aafeb23ee752c1
Brandon Castro <castroyou999@gmail.com>
2023-01-23 15:19:38 -0500

    commit tree c70929ab708023284b0ca11ae5d51b7615bc2b05

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+CREATE TABLE bears(
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER,
+sex TEXT,
+color TEXT,
+temperament TEXT,
+alive INTEGER
+);
+
+-- SELECT bears.name, bears.age FROM bears WHERE sex = "F";
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M" , "Brown" , "Relaxed", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M" , "White" , "Rowdy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F" , "Brown" , "Relaxed", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M" , "White" , "Sleepy", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 9, "F" , "Black" , "Relaxed", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 11, "M" , "Brown" , "Relaxed", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F" , "Black" , "Relaxed", 1);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL , 1, "M" , "Brown" , "Relaxed", 1);
